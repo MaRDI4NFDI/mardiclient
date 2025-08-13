4e9a48dbdc16de43cac67b58550379316877cdfe
Eloi Ferrer <ferrer@zib.de>
2025-08-13 08:38:38 +0000

    update sparql endpoint

--- mardiclient/mardi_config.py
+++ mardiclient/mardi_config.py
@@ -3,6 +3,6 @@ from typing import Dict
 config: Dict[str, str] = {
     'IMPORTER_API_URL': 'https://importer.portal.mardi4nfdi.de',
     'MEDIAWIKI_API_URL': 'https://portal.mardi4nfdi.de/w/api.php',
-    'SPARQL_ENDPOINT_URL': 'https://query.portal.mardi4nfdi.de/proxy/wdqs/bigdata/namespace/wdq/sparql',
+    'SPARQL_ENDPOINT_URL': 'https://query.portal.mardi4nfdi.de/sparql',
     'WIKIBASE_URL': 'https://portal.mardi4nfdi.de'
 }
