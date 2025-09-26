862254165020b3c992a4fabc34a81574150ac7f9
Eloi Ferrer <ferrer@zib.de>
2025-09-26 11:40:06 +0000

    fix typo

--- mardiclient/mardi_config.py
+++ mardiclient/mardi_config.py
@@ -4,6 +4,6 @@ config: Dict[str, str] = {
     'IMPORTER_API_URL': 'https://importer.portal.mardi4nfdi.de',
     'MEDIAWIKI_API_URL': 'https://portal.mardi4nfdi.de/w/api.php',
     'SPARQL_ENDPOINT_URL': 'https://query.portal.mardi4nfdi.de/sparql',
-    'WIKIBASE_URL': 'https://portal.mardi4nfdi.de'
+    'WIKIBASE_URL': 'https://portal.mardi4nfdi.de',
     'USER_AGENT': 'MaRDI client agent'
 }
